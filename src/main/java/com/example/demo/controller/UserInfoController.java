2d50dd0c9949357456a2775dde8a64a63cfee041
Zsolt Rokolya <rokyhu@gmail.com>
2021-10-03 21:15:25 +0200

    [fix] ClassCastException issue when returning logged user and roles

--- src/main/java/com/example/demo/controller/UserInfoController.java
+++ src/main/java/com/example/demo/controller/UserInfoController.java
@@ -2,9 +2,12 @@ package com.example.demo.controller;
 
 import com.example.demo.model.VehicleAppUser;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @RestController()
 public class UserInfoController {
@@ -12,7 +15,8 @@ public class UserInfoController {
     @GetMapping("/me")
     public String currentUser(){
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        VehicleAppUser user = (VehicleAppUser) authentication.getPrincipal();
-        return user.getUsername() + "\n" + user.getRoles();
+        String user = (String) authentication.getPrincipal();
+        Collection<? extends GrantedAuthority> roles = authentication.getAuthorities();
+        return user + "\n" + roles;
     }
 }
